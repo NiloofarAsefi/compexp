3cb67bfc8cc652c5b0f23de9fddb690ea34fa141
NiloofarAsefi <nasefi@ucsc.edu>
2024-11-04 22:51:42 +0000

    adjusting setting class for nli

--- nli/code/src/settings.py
+++ nli/code/src/settings.py
@@ -2,113 +2,227 @@
 It is an adaptation of the code referenced in:
 https://github.com/jayelm/compexp/blob/master/vision/settings.py"""
 
+# Change CCE_vision class setting to CCE_nli setting
+# class Settings:
+#     """
+#     Class that stores all the settings used in each run.
+#     """
+
+#     def __init__(
+#         self,
+#         *,
+#         subset,
+#         model,
+#         pretrained,
+#         beam_limit,
+#         device,
+#         dataset="places365",
+#         root_models="data/model/",
+#         root_datasets="data/dataset/",
+#         root_segmentations="data/cache/segmentations/",
+#         root_activations="data/cache/activations/",
+#         root_results="data/results/",
+#     ):
+#         self.index_subset = subset
+#         self.model = model
+#         self.dataset = dataset
+#         if pretrained == "places365" or pretrained == "imagenet":
+#             self.pretrained = pretrained
+#         else:
+#             self.pretrained = None
+#         self.num_clusters = num_clusters
+#         self.beam_limit = beam_limit
+#         self.dir_datasets = root_datasets
+#         self.__root_segmentations = root_segmentations
+#         self.__root_activations = root_activations
+#         self.__root_results = root_results
+#         self.__root_models = root_models
+#         self.device = device
+
+    
+
+
+#     def get_num_classes(self):
+#         """
+#         Returns the number of classes of the dataset.
+#         """
+#         if self.dataset == "places365":
+#             return 365
+#         elif self.dataset == "imagenet":
+#             return 1000
+#         elif self.dataset == "ade20k":
+#             return 365
+
+#     def get_model_file_path(self):
+#         """
+#         Returns the path to the pretrained weights of the model.
+#         """
+#         if self.pretrained == "places365":
+#             if self.model == "densenet161":
+#                 model_file_name = (
+#                     "whole_densenet161_places365_python36.pth.tar"
+#                 )
+#             else:
+#                 model_file_name = f"{self.model}_places365.pth.tar"
+#             return self.__root_models + "zoo/" + model_file_name
+#         elif self.pretrained == "imagenet":
+#             return None
+#         else:
+#             return "<UNTRAINED>"
+
+#     def get_weights(self):
+#         """
+#         Returns the pretrained weights of the model.
+#         """
+#         if self.pretrained == "imagenet":
+#             return "IMAGENET1K_V1"
+#         elif self.pretrained == "places365":
+#             return self.get_model_file_path()
+#         else:
+#             return None
+
+#     def get_data_directory(self):
+#         """
+#         Returns the directory where the data is stored.
+#         """
+#         if self.model != "alexnet":
+#             return f"{self.dir_datasets}broden1_224"
+#         else:
+#             return f"{self.dir_datasets}broden1_227"
+
+#     def get_model_root(self):
+#         """
+#         Returns the root directory where the models are stored.
+#         """
+#         return self.__root_models
+
+
+#     def get_parallel(self):
+#         """
+#         Returns True if the model is parallelized.
+#         """
+#         if self.dataset == "places365":
+#             return True
+#         else:
+#             return False
+
+    
+
+#     def get_activation_directory(self):
+#         """
+#         Returns the directory where the activations are stored.
+#         """
+#         return (
+#             f"{self.__root_activations}{self.model}/"
+#             f"{self.index_subset}/"
+#             f"{'w_'+self.pretrained if self.pretrained else 'untrained'}"
+#         )
+
+#     def get_results_directory(self):
+#         """
+#         Returns the directory where the results are stored.
+#         """
+#         return (
+#             f"{self.__root_results}{self.dataset}/{self.index_subset}/"
+#             f"{'w_'+self.pretrained if self.pretrained else 'untrained'}/"
+#             f"{self.model}"
+#         )
+
+#     def get_info_directory(self):
+#         """
+#         Returns the directory where the info files are stored.
+#         """
+#         return (
+#             f"{self.__root_segmentations}{self.dataset}/"
+#             f"{self.index_subset}/{self.get_img_size()}"
+#         )
+
+import os
 
 class Settings:
     """
-    Class that stores all the settings used in each run.
+    Class that stores all settings for clustered compositional explanations (CCE) in NLI tasks.
     """
-
     def __init__(
         self,
-        *,
         subset,
         model,
-        pretrained,
-        num_clusters,
-        beam_limit,
-        device,
-        dataset="places365",
+        model_type="bowman",  # Set a default model_type, like "bowman"
+        pretrained=None,
+        num_clusters=5,
+        beam_limit=10,
+        device="cpu",
+        dataset="snli",
         root_models="data/model/",
         root_datasets="data/dataset/",
-        root_segmentations="data/cache/segmentations/",
-        root_activations="data/cache/activations/",
         root_results="data/results/",
-    ):
+        metric="iou",
+        max_formula_length=5,
+        complexity_penalty=1.00,
+        parallel=4,
+        random_weights=False,
+        n_sentence_feats=2000,
+        data_file="data/analysis/snli_1.0_dev.feats",
+        shuffle=False,  # Add shuffle with a default value
+        debug=False  # Add debug with a default value
+    ):  
+        
+        # Core settings
         self.index_subset = subset
         self.model = model
+        self.model_type = model_type
         self.dataset = dataset
-        if pretrained == "places365" or pretrained == "imagenet":
-            self.pretrained = pretrained
-        else:
-            self.pretrained = None
+        self.pretrained = pretrained if pretrained in ["snli", "mnli"] else None
         self.num_clusters = num_clusters
         self.beam_limit = beam_limit
+        self.device = device
+        self.metric = metric
+        self.max_formula_length = max_formula_length
+        self.complexity_penalty = complexity_penalty
+        self.parallel = parallel
+        self.random_weights = random_weights
+        self.n_sentence_feats = n_sentence_feats
+        self.data_file = data_file
+        self.shuffle = shuffle
+        self.debug = debug
+        
+        # Root directories
         self.dir_datasets = root_datasets
-        self.__root_segmentations = root_segmentations
-        self.__root_activations = root_activations
         self.__root_results = root_results
         self.__root_models = root_models
-        self.device = device
 
-    def get_image_mean(self):
-        """
-        Returns the mean of the dataset.
-        """
-        if self.pretrained == "imagenet":
-            return [0.485, 0.456, 0.406]
-        elif self.pretrained == "places365":
-            return [0.485, 0.456, 0.406]
-        else:
-            return [0.5, 0.5, 0.5]
+        # Derived settings
+        self.vecpath = self._get_vecpath()
+        self.result_path = self._construct_result_path()
 
-    def get_image_stdev(self):
+    def _get_vecpath(self):
         """
-        Returns the standard deviation of the dataset.
+        Returns the path to the vector file based on the data file path.
         """
-        if self.pretrained == "imagenet":
-            return [0.229, 0.224, 0.225]
-        elif self.pretrained == "places365":
-            return [0.229, 0.224, 0.225]
-        else:
-            return [0.5, 0.5, 0.5]
+        assert self.data_file.endswith(".feats"), "Data file should have a .feats extension."
+        return self.data_file.replace(".feats", ".vec")
 
     def get_num_classes(self):
         """
-        Returns the number of classes of the dataset.
+        Returns the number of classes for the NLI dataset.
         """
-        if self.dataset == "places365":
-            return 365
-        elif self.dataset == "imagenet":
-            return 1000
-        elif self.dataset == "ade20k":
-            return 365
+        return 3  # entailment, neutral, contradiction
 
     def get_model_file_path(self):
         """
-        Returns the path to the pretrained weights of the model.
+        Returns the path to the pretrained model file for NLI.
         """
-        if self.pretrained == "places365":
-            if self.model == "densenet161":
-                model_file_name = (
-                    "whole_densenet161_places365_python36.pth.tar"
-                )
-            else:
-                model_file_name = f"{self.model}_places365.pth.tar"
-            return self.__root_models + "zoo/" + model_file_name
-        elif self.pretrained == "imagenet":
-            return None
-        else:
-            return "<UNTRAINED>"
+        model_file_name = f"{self.model}_{self.dataset}.pth.tar"
+        return f"{self.__root_models}{model_file_name}" if self.pretrained else None
 
-    def get_weights(self):
+    def get_results_directory(self):
         """
-        Returns the pretrained weights of the model.
+        Returns the directory where the clustering results are stored.
         """
-        if self.pretrained == "imagenet":
-            return "IMAGENET1K_V1"
-        elif self.pretrained == "places365":
-            return self.get_model_file_path()
-        else:
-            return None
-
-    def get_data_directory(self):
-        """
-        Returns the directory where the data is stored.
-        """
-        if self.model != "alexnet":
-            return f"{self.dir_datasets}broden1_224"
-        else:
-            return f"{self.dir_datasets}broden1_227"
+        return (
+            f"{self.__root_results}{self.dataset}/{self.index_subset}/"
+            f"{self.model}-clusters{self.num_clusters}/"
+        )
 
     def get_model_root(self):
         """
@@ -116,101 +230,49 @@ class Settings:
         """
         return self.__root_models
 
-    def get_img_size(self):
+    def _construct_result_path(self):
         """
-        Returns the size of the images.
+        Constructs and returns the path to save experiment results based on settings.
         """
-        if self.model != "alexnet":
-            return 224
-        else:
-            return 227
-
-    def get_mask_shape(self):
-        """
-        Returns the shape of the mask.
-        """
-        if self.model != "alexnet":
-            return (112, 112)
-        else:
-            return (113, 113)
-
-    def get_max_mask_size(self):
-        """
-        Returns the maximum size of the mask.
-        """
-        return self.get_mask_shape()[0] * self.get_mask_shape()[1]
-
-    def get_feature_names(self):
-        """
-        Returns the names of the layers that will be used to
-        extract the features.
-        """
-        if self.model == "resnet18":
-            return [
-                #  ['layer2', '0', 'conv1'], ['layer2', '0', 'conv2'],
-                #  ['layer2', '1', 'conv1'], ['layer2', '1', 'conv2'],
-                #  ['layer3', '0', 'conv1'], ['layer3', '0', 'conv2'],
-                #  ['layer3', '1', 'conv1'], ['layer3', '1', 'conv2'],
-                #  ['layer4', '0', 'conv1'], ['layer4', '0', 'conv2'],
-                # ["layer4", "1", "conv1"],
-                #  ["layer4", "1", "conv2"],
-                "layer4"
-            ]
-        elif self.model == "resnet50":
-            return ["layer4"]
-        elif self.model == "resnet101":
-            return ["layer4"]
-        elif self.model == "densenet161":
-            return ["features"]
-        elif self.model == "alexnet":
-            return ["features"]
-        elif self.model == "vgg16":
-            return ["features"]
-
-    def get_parallel(self):
-        """
-        Returns True if the model is parallelized.
-        """
-        if self.dataset == "places365":
-            return True
-        else:
-            return False
-
-    def get_segmentation_directory(self):
-        """
-        Returns the directory where the sparse segmentations
-        are stored.
-        """
+        mbase = os.path.splitext(os.path.basename(self.model))[0]
+        dbase = os.path.splitext(os.path.basename(self.data_file))[0]
         return (
-            f"{self.__root_segmentations}{self.dataset}/"
-            f"{self.index_subset}/{self.get_img_size()}/sparse"
+            f"exp/{dbase}-{mbase}-sentence-{self.max_formula_length}"
+            f"{'-shuffled' if self.shuffle else ''}"
+            f"{'-debug' if self.debug else ''}"
+            f"{f'-{self.metric}' if self.metric != 'iou' else ''}"
+            f"{'-random-weights' if self.random_weights else ''}"
         )
 
-    def get_activation_directory(self):
+    def get_clustering_settings(self):
         """
-        Returns the directory where the activations are stored.
+        Returns clustering-specific settings as a dictionary for easy access.
         """
-        return (
-            f"{self.__root_activations}{self.model}/"
-            f"{self.index_subset}/"
-            f"{'w_'+self.pretrained if self.pretrained else 'untrained'}"
-        )
+        return {
+            "num_clusters": self.num_clusters,
+            "metric": self.metric,
+            "max_formula_length": self.max_formula_length,
+            "complexity_penalty": self.complexity_penalty,
+        }
 
-    def get_results_directory(self):
+    def print_settings(self):
         """
-        Returns the directory where the results are stored.
+        Prints all current settings for verification.
         """
-        return (
-            f"{self.__root_results}{self.dataset}/{self.index_subset}/"
-            f"{'w_'+self.pretrained if self.pretrained else 'untrained'}/"
-            f"{self.model}"
-        )
-
-    def get_info_directory(self):
-        """
-        Returns the directory where the info files are stored.
-        """
-        return (
-            f"{self.__root_segmentations}{self.dataset}/"
-            f"{self.index_subset}/{self.get_img_size()}"
-        )
+        print("Current NLI CCE Settings:")
+        print(f"Subset: {self.index_subset}")
+        print(f"Model: {self.model}")
+        print(f"Pretrained: {self.pretrained}")
+        print(f"Number of Clusters: {self.num_clusters}")
+        print(f"Beam Limit: {self.beam_limit}")
+        print(f"Device: {self.device}")
+        print(f"Dataset: {self.dataset}")
+        print(f"Metric: {self.metric}")
+        print(f"Max Formula Length: {self.max_formula_length}")
+        print(f"Complexity Penalty: {self.complexity_penalty}")
+        print(f"Parallel Processing: {self.parallel}")
+        print(f"Random Weights: {self.random_weights}")
+        print(f"Sentence Features: {self.n_sentence_feats}")
+        print(f"Data File: {self.data_file}")
+        print(f"Vector Path: {self.vecpath}")
+        print(f"Results Directory: {self.get_results_directory()}")
